95395c7e4e544f6d87bb6b78989159a3c0866874
Belladonnask <kauancosta507@gmail.com>
2026-06-01 12:37:24 -0300

    Update core.py

---
+++ ultimate_ssrf/core.py
@@ -0,0 +1,23 @@
+from ssrf_arsenal import (
+    SSRFEvidence,
+    DiscoveredEndpoint,
+    TargetManager,
+    ProxyManager,
+    LLMClient,
+    AISkills,
+    WAFFingerprinter,
+    UltimateSSRFFramework,
+    setup_argparse,
+)
+
+__all__ = [
+    "SSRFEvidence",
+    "DiscoveredEndpoint",
+    "TargetManager",
+    "ProxyManager",
+    "LLMClient",
+    "AISkills",
+    "WAFFingerprinter",
+    "UltimateSSRFFramework",
+    "setup_argparse",
+]
